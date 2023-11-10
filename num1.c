13b2eebad8e481a77738e77d7ca5bba4afa12a31
Késia Silva <kesiasilvab6@gmail.com>
2023-11-10 10:28:38 -0300

    Create num1.c

---
+++ num1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*1. **Soma de elementos em um array**: dado o int array[5] =
+{1,2,3,4,5}, Escreva um programa que calcule a soma de todos
+os elementos em um array de inteiros.*/
+
+int main(){
+    int num[5]= {1, 2, 3, 4, 5}, soma = 0;
+
+    
+        soma = num[0] + num[1] + num[2] + num[3] + num[4];
+    
+
+    printf("a soma dos arraya = %i", soma);
+}
