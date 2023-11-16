0a325c33bd833a805fe52bc6b6a75127b4745c06
Késia Silva <kesiasilvab6@gmail.com>
2023-11-16 16:37:07 -0300

    Update num4.c

--- num4.c
+++ num4.c
@@ -21,6 +21,26 @@ int main(){
 
     printf("tudo junto: \n");
     puts(s);
-
-
 }
+
+/*resposta do professor:
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+	char str1[100], str2[100], resultado[200];
+	
+	printf("Digite a primeira string: ");
+	fgets(str1, sizeof(str1), stdin);
+	
+	printf("Digite a segunda string: ");
+	fgets(str2, sizeof(str2), stdin);
+	
+	strcpy(resultado, str1);
+	strcat(resultado, str2);
+	printf("String concatenada: %s", resultado);
+}
+
+*/
