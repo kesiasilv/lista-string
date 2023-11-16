b42415a1b16cfb0e148cc2bb0df56a58a6f0928b
Késia Silva <kesiasilvab6@gmail.com>
2023-11-16 16:30:00 -0300

    Update num2.c

--- num2.c
+++ num2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /*2. **Média de elementos em um array**:dado o float array[] =
 {1.5, 2.5, 3.5, 4.5, 5.5} Escreva um programa que calcule a
@@ -14,3 +15,26 @@ int main(){
 
     printf("a soma dos arraya = %.2f", soma / 5);
 }
+
+/*//resposta do professor:
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	
+	float array[] = {1.5, 2.5, 3.5, 4.5, 5.5};
+	int n = sizeof(array) / sizeof(array[0]);
+	int i;
+	float soma = 0;
+	float media = 0;
+	
+	
+	for (i = 0; i < n; i++){
+	soma += array[i];
+	}
+	
+	media = soma / n;
+	printf("A media dos elementos e: %.2f\n", media);
+	
+}*/
